3325393304eaefbaae9f866126fdfb3bd3be6c6a
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-01 14:15:31 +0200

    start oefening 3

--- src/main/java/be/vdab/music/SaveAlbum.java
+++ src/main/java/be/vdab/music/SaveAlbum.java
@@ -1,4 +1,35 @@
 package be.vdab.music;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
 public class SaveAlbum {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        try {
+            emf = Persistence.createEntityManagerFactory("create");
+            em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            Album albumOne = new Album();
+
+            albumOne.setTitle("See Without Eyes");
+            albumOne.setAuthor("The Glitch Mob");
+            albumOne.setTracks();
+
+            tx.commit();
+            System.out.println("Menu Saved");
+
+        } finally {
+            if (em != null)
+                em.close();
+            if (emf != null)
+                emf.close();
+        }
+    }
 }
---
+++ src/main/java/be/vdab/music/Tracks.java
@@ -0,0 +1,8 @@
+package be.vdab.music;
+
+public class Tracks {
+    private int albumId;
+    private String trackTitle;
+
+
+}
