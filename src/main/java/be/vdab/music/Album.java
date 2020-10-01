07aa5ade87c257dd35a320251bb824033383c6af
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-01 14:05:44 +0200

    doornemen voorbeelden

---
+++ src/main/java/be/vdab/music/Album.java
@@ -0,0 +1,50 @@
+package be.vdab.music;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String author;
+
+    @ElementCollection
+    private List<String> tracks = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<String> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<String> tracks) {
+        this.tracks = tracks;
+    }
+}
